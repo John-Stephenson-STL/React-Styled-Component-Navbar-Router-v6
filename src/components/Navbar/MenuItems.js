03e19b327d48c322e0130fcfc081a8b5d6ae80d2
John.Stephenson.STL <John.Stephenson.STL@gmail.com>
2020-09-14 07:51:55 -0500

    first commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: '#',
+    name: 'nav-links',
+  },
+  {
+    title: 'Services',
+    url: '#',
+    name: 'nav-links',
+  },
+  {
+    title: 'Product',
+    url: '#',
+    name: 'nav-links',
+  },
+  {
+    title: 'Contact Us',
+    url: '#',
+    name: 'nav-links',
+  },
+  {
+    title: 'Sign up',
+    url: '#',
+    name: 'nav-links-mobile',
+  },
+]
